863672608ac8d637533986b1165f5c92a0213973
Chris Griffing <chris.griffing@gitkraken.com>
2024-09-24 16:42:13 -0700

    up to date

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,3 +1,9 @@
-declare module 'main' {
-    export function greet(): I32;
+declare module "main" {
+  export function greet(): I32;
+}
+
+declare module "extism:host" {
+  interface user {
+    notify(ptr: PTR): PTR;
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,22 @@
+// import "@extism/js-pdk"
+
+const { notify } = Host.getFunctions()
+
 export function greet() {
-  Host.outputString(`Hello, ${Host.inputString()}`)
+  const name = Host.inputString();
+  Host.outputString(`Hello, ${name}`)
+
+  // const request: HttpRequest = {
+  //   method: "POST" as const,
+  //   url: "https://localhost/echo",
+  // }
+  // const response = Http.request(request, JSON.stringify({
+  //   name
+  // }))
+
+  const message = "Will it blend?"
+  const mem = Memory.fromString(message)
+
+  notify(mem.offset)
+
 }
